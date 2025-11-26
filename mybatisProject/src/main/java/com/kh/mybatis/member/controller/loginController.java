d375c099edc36d72989e3ee5153f4cd37c865f5e
KJH-15123 <kim50504376@gmail.com>
2025-11-26 17:45:50 +0900

    1126

--- mybatisProject/src/main/java/com/kh/mybatis/member/controller/loginController.java
+++ mybatisProject/src/main/java/com/kh/mybatis/member/controller/loginController.java
@@ -1,11 +1,14 @@
 package com.kh.mybatis.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.mybatis.member.model.vo.Member;
 
 /**
  * Servlet implementation class loginController
@@ -41,6 +44,14 @@ public class loginController extends HttpServlet {
 		String userPwd = request.getParameter("userPwd");
 		
 		System.out.println(userId+"/"+userPwd);
+		
+		//service에 데이터 전달 및 요청
+		//마이바티스 구문은 매개변수 전달이 하나만 가능하기때문에 데이터가 여러개일 경우 하나의 묶음으로 처리하여 요청하기
+		Member m = new Member();
+		m.setUserId(userId);
+		m.setUserPwd(userPwd);
+		
+		
 		
 		
 		
--- mybatisProject/src/main/java/com/kh/mybatis/member/model/service/MemberService.java
+++ mybatisProject/src/main/java/com/kh/mybatis/member/model/service/MemberService.java
@@ -1,5 +1,10 @@
 package com.kh.mybatis.member.model.service;
 
+//연결객체 담당 클래스
 public class MemberService {
+	
+	
+	
+	
 
 }
